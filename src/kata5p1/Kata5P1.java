d7c2f6311c481aebaacbb5bdabca6fc764dda006
david-koschel <113340344+david-koschel@users.noreply.github.com>
2023-01-15 14:40:34 +0000

    Version 2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,12 +1,21 @@
 package kata5p1;
 
+import org.sqlite.core.DB;
+
 import java.sql.*;
 
 public class Kata5P1 {
 
+    private static final String DBUrl = "jdbc:sqlite:KATA5.db";
+
     public static void main(String[] args) {
+        getAll();
+        newTable();
+    }
+
+    public static void getAll() {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:KATA5.db");
+            Connection conn = DriverManager.getConnection(DBUrl);
             if (conn != null) {
                 ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM PEOPLE");
                 while (rs.next()) {
@@ -19,6 +28,15 @@ public class Kata5P1 {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void newTable() {
+        try (Connection conn = DriverManager.getConnection(DBUrl)) {
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS EMAIL (Id INTEGER PRIMARY KEY AUTOINCREMENT, Mail text NOT NULL)");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
